f5a00c022475620b9c8269dff8b1c5e1e23e6893
David <mellod98@gmail.com>
2025-09-24 22:25:04 -0300

    teste para o render1 port

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,9 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // BUSCAR DEVEDORES POR USUÁRIO
 app.get("/devedores", async (req, res) => {
